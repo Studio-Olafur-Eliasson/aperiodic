18f6ffc1935b9714c72aa9781c787ef68469d0d4
Claire Djang <clairedjang@gmail.com>
2026-01-30 14:19:54 +0100

    Added resources / icons

--- AperiodicComponent.cs
+++ AperiodicComponent.cs
@@ -65,7 +65,7 @@ namespace Aperiodic
         /// You can add image files to your project resources and access them like this:
         /// return Resources.IconForThisComponent;
         /// </summary>
-        protected override System.Drawing.Bitmap Icon => null;
+        protected override System.Drawing.Bitmap Icon => Resources.aperiodic4tile24px;
 
         /// <summary>
         /// Each component must have a unique Guid to identify it. 
